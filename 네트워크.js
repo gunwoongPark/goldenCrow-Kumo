d3023c7a69093c0c0f56b0f4c7ca7eefadf9f8bb
gunwoongPark <bung1438@gmail.com>
2020-08-31 20:52:38 +0900

    네트워크 구현 완료

---
+++ 네트워크.js
@@ -0,0 +1,36 @@
+
+function DFS(start, graph, visit) {
+    if (visit[start] === true) {
+        return 0;
+    }
+
+    visit[start] = true;
+
+    for (let idx = 0; idx < graph[start].length; ++idx) {
+
+
+        if (graph[start][idx] === 1) {
+            DFS(idx, graph, visit)
+        }
+    }
+
+    return 1;
+}
+
+function solution(n, computers) {
+    let visit = new Array();
+    var answer = 0;
+
+    for (let idx = 0; idx < n; ++idx)
+        visit.push(false);
+
+    for (let idx = 0; idx < n; ++idx) {
+        if (DFS(idx, computers, visit))
+            answer++;
+
+    }
+
+    return answer;
+}
+
+console.log(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
